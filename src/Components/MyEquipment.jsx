f5a4db988de36b308a286f349b64cc98007e1e06
Fahima-anny <fahima.info03@gmail.com>
2024-12-05 22:13:40 +0600

    delete method added

--- src/Components/MyEquipment.jsx
+++ src/Components/MyEquipment.jsx
@@ -1,10 +1,12 @@
 import { useLoaderData } from "react-router-dom";
 import MyEquitmentsCard from "./sections/MyEquitmentsCard";
+import { useState } from "react";
 
 
 const MyEquipment = () => {
 
-const allData = useLoaderData() ;
+const loadedData = useLoaderData() ;
+const [allData,setAllData] = useState(loadedData)
 console.log(allData)
     return (
         <div>
@@ -14,7 +16,13 @@ console.log(allData)
 
 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
     {
-        allData.map(data => <MyEquitmentsCard data={data} key={data._id}></MyEquitmentsCard>)
+        allData?.map(data =>
+            <MyEquitmentsCard
+             data={data}
+              setAllData={setAllData}
+               allData={allData} 
+               key={data._id}
+               ></MyEquitmentsCard>)
     }
 </div>
 
--- src/Components/sections/MyEquitmentsCard.jsx
+++ src/Components/sections/MyEquitmentsCard.jsx
@@ -5,9 +5,27 @@ import { MdOutlineDelete } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 
-const MyEquitmentsCard = ({data}) => {
+const MyEquitmentsCard = ({data,allData,setAllData}) => {
 
     const {_id,item, category,price,photo,rating,delivery} = data
+
+const handleDelete = () => {
+    console.log("delete : ",_id) ;
+    fetch(`http://localhost:5000/equipments/id/${_id}`, {
+        method: "DELETE"
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        if(data.deletedCount > 0){
+            alert("deleted") ;
+            console.log(allData)
+            const remaining = allData.filter(d => d._id !== _id) ;
+            console.log(remaining)
+            setAllData(remaining) ;
+        }
+    })
+}
 
     return (
         <div className="card bg-base-200 rounded-lg">
@@ -32,7 +50,12 @@ const MyEquitmentsCard = ({data}) => {
       to={`/updateProduct/${_id}`}
        className="flex gap-2 items-center btn bg-[#ffa400] hover:bg-[#cc8500] text-black">
         Update <FaEdit className="text-xl" /></Link>
-        <button className="btn bg-[#ffa400] hover:bg-[#ce8600] text-black flex items-center gap-1">Delete <MdOutlineDelete  className="text-2xl" /></button>
+        <button 
+        className="btn bg-[#ffa400] hover:bg-[#ce8600] text-black flex items-center gap-1"
+        onClick={handleDelete}
+        >Delete
+         <MdOutlineDelete  className="text-2xl" />
+         </button>
           </div>
         </div>
       </div>
